4842f79030798f7d90c61923931a9c4866836b4e
Philipuden <philip.uden@elev.ga.ntig.se>
2024-09-16 13:56:07 +0200

    edit till cart db klar

--- lib/pluggy/controllers/cart_controller.ex
+++ lib/pluggy/controllers/cart_controller.ex
@@ -1,4 +1,6 @@
 defmodule Pluggy.CartController do
+  require IEx
+
   alias Pluggy.Cart
   import Pluggy.Template, only: [render: 2]
   import Plug.Conn, only: [send_resp: 3]
@@ -24,6 +26,43 @@ defmodule Pluggy.CartController do
         "",
         false,
         true
+      ])
+
+    redirect(conn, "/menu")
+  end
+
+  def add_edit(conn, id, params) do
+    IO.puts("Adding to cart")
+
+    ## Get cart_id from session ##
+    cart_id = conn.private.plug_session["cart"]
+
+    pizza_ingredients = Postgrex.query!(DB, "SELECT containing_ingredients FROM pizzas WHERE id = $1 LIMIT 1", [String.to_integer(id)]).rows
+    pizza_ingredients2 = hd(hd(pizza_ingredients))
+    add_ingredients = (params["ingredients"] -- pizza_ingredients2)
+    remove_ingredients = (pizza_ingredients2 -- params["ingredients"])
+
+
+
+
+    # ## Update cart in DB ##
+    # Cart.update_cart(new_cart)
+    Postgrex.query!(
+      DB,
+      "INSERT INTO carts (uuid, pizza_id, add_ingredients, remove_ingredients, gluten, size) VALUES($1, $2,$3, $4, $5, $6)",
+      [
+        cart_id,
+        id,
+        add_ingredients,
+        remove_ingredients,
+        case params["gluten"] do
+          "glutenfri" -> true
+          _ -> false
+        end,
+        case params["pizza_size"] do
+          "familj" -> false
+          _ -> true
+        end
       ])
 
     redirect(conn, "/menu")
--- lib/pluggy/models/cart.ex
+++ lib/pluggy/models/cart.ex
@@ -3,21 +3,13 @@ defmodule Pluggy.Cart do
     id: nil,
     uuid: "",
     pizza_id: "",
-    add_ingridients: "",
-    remove_ingridients: "",
+    add_ingredients: "",
+    remove_ingredients: "",
     size: "",
     gluten: ""
   )
 
   alias Pluggy.Cart
-
-  def add_to_cart(id) do
-    Postgrex.query!(
-      DB,
-      "INSERT INTO cart (uuid, pizza_id, gluten, size) VALUES($1, $2,$3, $4,)",
-      id
-    )
-  end
 
   def all(conn) do
     Postgrex.query!(DB, "SELECT * FROM carts WHERE uuid = $1", [conn.private.plug_session["cart"]]).rows
@@ -25,13 +17,13 @@ defmodule Pluggy.Cart do
   end
 
   def to_struct_list(rows) do
-    for [id, uuid, pizza_id, add_ingridients, remove_ingridients, size, gluten] <- rows,
+    for [id, uuid, pizza_id, add_ingredients, remove_ingredients, size, gluten] <- rows,
         do: %Cart{
           id: id,
           uuid: uuid,
           pizza_id: pizza_id,
-          add_ingridients: add_ingridients,
-          remove_ingridients: remove_ingridients,
+          add_ingredients: add_ingredients,
+          remove_ingredients: remove_ingredients,
           size: size,
           gluten: gluten
         }
