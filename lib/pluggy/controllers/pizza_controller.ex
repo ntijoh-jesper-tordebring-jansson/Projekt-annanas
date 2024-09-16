0aab5166d0950daacd75457e4eb0fd2fcf96c25a
Philipuden <philip.uden@elev.ga.ntig.se>
2024-09-16 10:16:48 +0200

    uuid funkar och kop knappar funkar

--- lib/pluggy/controllers/pizza_controller.ex
+++ lib/pluggy/controllers/pizza_controller.ex
@@ -1,14 +1,16 @@
 defmodule Pluggy.PizzaController do
-  # require IEx
+  require IEx
 
   alias Pluggy.Pizza
   import Pluggy.Template, only: [render: 2]
   import Plug.Conn, only: [send_resp: 3]
 
   def index(conn) do
-    if conn.cookies["cart"] == nil do
-      Map.put(conn.cookies, "cart", UUID.uuid4)
+    if conn.private.plug_session["user_id"] == nil do
+      Plug.Conn.put_session(conn, :cart, UUID.uuid4())
+      |> redirect("/") #skicka vidare modifierad conn
     end
+
     send_resp(conn, 200, render("pizzas/index", data: []))
   end
 
@@ -25,7 +27,7 @@ defmodule Pluggy.PizzaController do
     send_resp(conn, 200, render("pizzas/edit", pizza: Pizza.get(id), ingredients: Pizza.ingredients))
   end
 
-  # defp redirect(conn, url) do
-  #   Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
-  # end
+  defp redirect(conn, url) do
+    Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
+  end
 end
--- lib/pluggy/models/cart.ex
+++ lib/pluggy/models/cart.ex
@@ -1,10 +1,5 @@
 defmodule Pluggy.Cart do
-  def add_to_cart(id) do
-
-    Postgrex.query!(DB, "INSERT INTO cart (uuid, pizza_id, gluten, size) VALUES($1, $2,$3, $4,)", id )
-  end
-
   def all(uuid) do
-    
+
   end
 end
